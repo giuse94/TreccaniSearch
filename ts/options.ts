8bad603dcd29f85d40cd32a1efa229a7169b4bf4
Giuse <59248203+giuse94@users.noreply.github.com>
2020-09-28 14:38:34 +0200

    Create options.ts (can't use inline scripts)

---
+++ ts/options.ts
@@ -0,0 +1,14 @@
+// Restore user preference or default option value when opening the options page.
+document.addEventListener("DOMContentLoaded", () => {
+  browser.storage.local.get("alwaysNewTab").then((item: { [key: string]: boolean }) => {
+    let checkBoxElement = document.getElementById("alwaysNewTabOption") as HTMLInputElement;
+    checkBoxElement.checked = item?.alwaysNewTab ?? true;
+  });
+});
+
+// Store user preference when submitting the form (by clicking on "Save" button).
+document.querySelector("button")!.addEventListener("click", () => {
+  let checkBoxElement = document.getElementById("alwaysNewTabOption") as HTMLInputElement;
+  let optionValue = checkBoxElement.checked ? true : false;
+  browser.storage.local.set({alwaysNewTab: optionValue});
+});
