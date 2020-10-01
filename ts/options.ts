e0bc33ad58d6b86f653d0f0dafa0aa7dea9d3865
Giuse <59248203+giuse94@users.noreply.github.com>
2020-10-01 16:04:30 +0200

    Update options.ts
    Store and retrieve value of option to open new tabs in background.
    Give variables more specific names.

--- ts/options.ts
+++ ts/options.ts
@@ -1,14 +1,21 @@
 // Restore user preference or default option value when opening the options page.
 document.addEventListener("DOMContentLoaded", () => {
-  browser.storage.local.get("alwaysNewTab").then( (item: { [key: string]: boolean }) => {
-    let checkBoxElement = document.getElementById("alwaysNewTabOption") as HTMLInputElement;
-    checkBoxElement.checked = item?.alwaysNewTab ?? true;
+  browser.storage.local.get(["alwaysNewTab", "backgroundNewTab"]).then( (item: { [key: string]: boolean }) => {
+    let alwaysNewTab_CBEl = document.getElementById("alwaysNewTabOption") as HTMLInputElement;
+    alwaysNewTab_CBEl.checked = item?.alwaysNewTab ?? true;
+    let backgroundNewTab_CBEl = document.getElementById("backgroundNewTabOption") as HTMLInputElement;
+    backgroundNewTab_CBEl.checked = item?.backgroundNewTab ?? false;
   });
 });
 
 // Store user preference when submitting the form (by clicking on "Save" button).
 document.querySelector("button")!.addEventListener("click", () => {
-  let checkBoxElement = document.getElementById("alwaysNewTabOption") as HTMLInputElement;
-  let optionValue = checkBoxElement.checked ? true : false;
-  browser.storage.local.set({alwaysNewTab: optionValue});
+  let alwaysNewTab_CBEl = document.getElementById("alwaysNewTabOption") as HTMLInputElement;
+  let alwaysNewTabOptionValue = alwaysNewTab_CBEl.checked ? true : false;
+  let backgroundNewTab_CBEl = document.getElementById("backgroundNewTabOption") as HTMLInputElement;
+  let backgroundNewTabOptionValue = backgroundNewTab_CBEl.checked ? true : false;
+  browser.storage.local.set({
+    alwaysNewTab: alwaysNewTabOptionValue,
+    backgroundNewTab: backgroundNewTabOptionValue
+  });
 });
